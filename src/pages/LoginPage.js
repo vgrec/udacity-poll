b176ecb3ec92cbfa2f9c64832473936d87054bea
Veaceslav Grec <grec.vs@gmail.com>
2024-01-13 16:34:11 +0100

    Make the form controllable

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,20 +3,27 @@ import DropdownMenu from "../components/DropdownMenu";
 import appLogo from "../images/app_logo.png";
 
 const LoginPage = ({ users }) => {
-  const [selectedUser, setSelectedUser] = useState(users[0]);
-
-  console.log("users", users);
-  console.log("selected", selectedUser);
+  const [username, setUsername] = useState(users[0].id);
+  const [password, setPassword] = useState(users[0].password);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log("submit");
+    console.log("loggin with: ", username, password);
   };
 
   const onOptionsSelected = (userId) => {
     const user = users.find((user) => user.id === userId);
-    setSelectedUser(user);
+    setUsername(user.id);
+    setPassword(user.password);
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
   };
 
   return (
@@ -24,21 +31,22 @@ const LoginPage = ({ users }) => {
       <form onSubmit={handleSubmit}>
         <h2>Employee Polls</h2>
 
-        <img
-          src={appLogo}
-          alt="App Logo"
-          width="250px"
-          height="250px"
-        />
+        <img src={appLogo} alt="App Logo" width="250px" height="250px" />
 
-        <strong style={{marginTop: 16}}>Choose a test user to login</strong>
+        <strong style={{ marginTop: 16 }}>Choose a test user to login</strong>
         <DropdownMenu users={users} onOptionSelected={onOptionsSelected} />
         <br />
-        <input type="text" placeholder="Username" value={selectedUser.id} />
         <input
+          type="text"
+          onChange={handleUsernameChange}
+          placeholder="Username"
+          value={username}
+        />
+        <input
+          onChange={handlePasswordChange}
           type="password"
           placeholder="Password"
-          value={selectedUser.password}
+          value={password}
         />
         <button>Login</button>
       </form>
