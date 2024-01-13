dded716dd2181c2b7517b6eeaddf04b2299b0381
Veaceslav Grec <grec.vs@gmail.com>
2024-01-13 20:47:03 +0100

    add avatar icon to leaderboard table

--- src/_DATA.js
+++ src/_DATA.js
@@ -3,7 +3,7 @@ let users = {
       id: 'sarahedo',
       password:'password123',
       name: 'Sarah Edo',
-      avatarURL: null,
+      avatarURL: 'https://cdn3.vectorstock.com/i/thumbs/67/37/people-face-icons-29-vector-14866737.jpg',
       answers: {
         "8xf0y6ziyjabvozdd253nd": 'optionOne',
         "6ni6ok3ym7mf1p33lnez": 'optionOne',
@@ -16,7 +16,7 @@ let users = {
       id: 'tylermcginnis',
       password:'abc321',
       name: 'Tyler McGinnis',
-      avatarURL: null,
+      avatarURL: 'https://cdn3.vectorstock.com/i/thumbs/67/02/people-face-icons-22-vector-14866702.jpg',
       answers: {
         "vthrdm985a262al8qx3do": 'optionOne',
         "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -27,7 +27,7 @@ let users = {
       id: 'mtsamis',
       password:'xyz123',
       name: 'Mike Tsamis',
-      avatarURL: null,
+      avatarURL: 'https://cdn2.vectorstock.com/i/thumbs/68/86/people-face-icons-17-vector-14836886.jpg',
       answers: {
         "xj352vofupe1dqz9emx13r": 'optionOne',
         "vthrdm985a262al8qx3do": 'optionTwo',
@@ -39,7 +39,7 @@ let users = {
       id: 'zoshikanlu',
       password:'pass246',
       name: 'Zenobia Oshikanlu',
-      avatarURL: null,
+      avatarURL: 'https://cdn2.vectorstock.com/i/thumbs/67/46/people-face-icons-9-vector-14836746.jpg',
       answers: {
         "xj352vofupe1dqz9emx13r": 'optionOne',
       },
--- src/components/LeaderboardTable.js
+++ src/components/LeaderboardTable.js
@@ -12,7 +12,20 @@ const LeaderboardTable = ({ users }) => {
         <tbody>
           {users.map((user) => (
             <tr key={user.id}>
-              <td>{user.name}</td>
+              <td>
+                <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                  <img
+                    width="50"
+                    height="50"
+                    src={user.avatarURL}
+                    alt={`Avatar of ${user.name}`}
+                  />
+                  <div style={{ display: "flex", flexDirection: "column", marginLeft: 8 }}>
+                    <strong>{user.name}</strong>
+                    <div style={{fontSize: 13, color: "#424242", marginTop: 2}}>{user.id}</div>
+                  </div>
+                </div>
+              </td>
               <td>{Object.keys(user.answers).length}</td>
               <td>{user.questions.length}</td>
             </tr>
