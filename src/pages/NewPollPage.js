02362418c9612ef242893f1f5754c8f6825028d5
Veaceslav Grec <grec.vs@gmail.com>
2024-01-13 18:22:29 +0100

    add state for poll fields

--- src/pages/NewPollPage.js
+++ src/pages/NewPollPage.js
@@ -1,15 +1,37 @@
+import React, { useState } from "react";
+
 const NewPollPage = () => {
+  const [optionOne, setOptionOne] = useState("");
+  const [optionTwo, setOptionTwo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("optionOne: ", optionOne);
+    console.log("optionTwo: ", optionTwo);
+  };
+
   return (
     <div>
       <p className="center-text">Create new poll</p>
       <div className="poll-container">
         <h2 className="center-text">Would you rather?</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <strong>First option</strong>
-          <input type="text" placeholder="Option One" />
+          <input
+            onChange={(e) => setOptionOne(e.target.value)}
+            value={optionOne}
+            type="text"
+            placeholder="First option"
+          />
 
           <strong>Second option</strong>
-          <input type="text" placeholder="Option Two" />
+          <input
+            onChange={(e) => setOptionTwo(e.target.value)}
+            value={optionTwo}
+            type="text"
+            placeholder="Second option"
+          />
           <button>Create Poll</button>
         </form>
       </div>
