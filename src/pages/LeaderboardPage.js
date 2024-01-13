8e7e617f0447e9ba800404aa1f915e1e1c1a239b
Veaceslav Grec <grec.vs@gmail.com>
2024-01-13 18:55:38 +0100

    sort users

--- src/pages/LeaderboardPage.js
+++ src/pages/LeaderboardPage.js
@@ -1,13 +1,12 @@
 import LeaderboardTable from "../components/LeaderboardTable";
 
 const LeaderboardPage = ({ users }) => {
-  const sortedUsers = users;
-  //   Object.values(users).sort(
-  //     (a, b) =>
-  //       Object.keys(b.answers).length +
-  //       b.questions.length -
-  //       (Object.keys(a.answers).length + a.questions.length)
-  //   );
+  const sortedUsers = users.sort(
+    (a, b) =>
+      Object.keys(b.answers).length +
+      b.questions.length -
+      (Object.keys(a.answers).length + a.questions.length)
+  );
 
   return <LeaderboardTable users={sortedUsers} />;
 };
