eed1a2a8cbdf6e8a49e58d8606226dc644f6ac50
Veaceslav Grec <grec.vs@gmail.com>
2024-01-15 22:01:04 +0100

    init redux

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer, middleware);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
