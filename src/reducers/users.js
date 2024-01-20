1884570b503f2daa7eb1fa78eaa13066473de183
Veaceslav Grec <grec.vs@gmail.com>
2024-01-20 07:24:55 +0100

    update user questions

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,17 @@
 import { RECEIVE_USERS, SAVE_USER_ANSWER } from "../actions/users";
+import { ADD_QUESTION } from "../actions/questions";
 
 export default function users(state = {}, action) {
   switch (action.type) {
+    case ADD_QUESTION:
+      const { question } = action;
+      return {
+        ...state,
+        [question.author]: {
+          ...state[question.author],
+          questions: [...state[question.author].questions, question.id],
+        },
+      };
     case RECEIVE_USERS:
       return {
         ...state,
